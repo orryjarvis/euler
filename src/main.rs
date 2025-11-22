d640ff655572e940a09e1104f8a34472cf33555e
Orry Jarvis <orryjarvis@gmail.com>
2025-11-22 09:37:37 -0500

    p0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", p0(554_000));
+}
+
+fn p0(n: u128) -> u128 {
+    (1..=n)
+        .map(|x| x * x)
+        .filter(|x| x % 2 == 1)
+        .sum()
 }
