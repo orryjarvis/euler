2ac8ad58b6cad6bd39469be346c4cf1fdac84f60
Orry Jarvis <orryjarvis@gmail.com>
2025-11-23 07:28:59 -0500

    p3

--- src/lib.rs
+++ src/lib.rs
@@ -25,4 +25,41 @@ pub mod lib {
         fib_map.insert(n, computed);
         computed
     }
+
+    pub fn is_prime(n: u128) -> bool {
+        if n <= 1 {
+            return false;
+        }
+        let upper: u128 = f64::floor(f64::sqrt(n as f64)) as u128;
+        (2..=upper)
+            .filter(|x| n % x == 0)
+            .collect::<Vec<_>>()
+            .is_empty()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_fib() {
+            assert_eq!(1, fib(1));
+            assert_eq!(2, fib(2));
+            assert_eq!(8, fib(5));
+            assert_eq!(3, fib(3));
+            assert_eq!(5, fib(4));
+        }
+
+        
+        #[test]
+        fn test_is_prime() {
+            assert_eq!(false, is_prime(0));
+            assert_eq!(false, is_prime(1));
+            assert_eq!(true, is_prime(2));
+            assert_eq!(true, is_prime(3));
+            assert_eq!(false, is_prime(4));
+            assert_eq!(true, is_prime(5));
+            assert_eq!(false, is_prime(6));
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use euler::lib;
 
 fn main() {
-    println!("{:?}", p0(554_000));
-    println!("{:?}", p1(1000));
-    println!("{:?}", p2(4_000_000));
+    println!("p0: {:?}", p0(554_000));
+    println!("p1: {:?}", p1(1000));
+    println!("p2: {:?}", p2(4_000_000));
+    println!("p3: {:?}", p3(600_851_475_143));
 }
 
 fn p0(n: u128) -> u128 {
@@ -28,4 +29,12 @@ fn p2(n: u128) -> u128 {
         .filter(|x| x <= &n)
         .filter(|x| x % 2 == 0)
         .sum()
+}
+
+fn p3(n: u128) -> u128 {
+    let upper: u128 = f64::floor(f64::sqrt(n as f64)) as u128;
+    (1..=upper)
+        .filter(|x| n % x == 0)
+        .filter(|x| lib::is_prime(*x))
+        .last().unwrap()
 }
