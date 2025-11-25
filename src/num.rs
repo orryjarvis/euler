6f62c76678379111aab88819b57ba5468141d7b1
Orry Jarvis <orryjarvis@gmail.com>
2025-11-24 20:54:06 -0500

    prime/fib generators

--- src/num.rs
+++ src/num.rs
@@ -7,6 +7,9 @@
 //         Mutex::new(HashMap::new())
 //     };
 // }
+use std::mem;
+
+use crate::generator::{create_generator};
 
 pub fn fib(n: i32) -> u128 {
     let computed = match n {
@@ -22,6 +25,22 @@ pub fn fib_largest_lte(n: u128) -> i32 {
     x.log(std::f32::consts::PHI).floor() as i32
 }
 
+pub fn fib_iterator() -> impl Iterator<Item=u128> {
+    let c = #[coroutine] || {
+        yield 0;
+        yield 1;
+        let mut a  = 0_u128;
+        let mut b = 1_u128;
+        loop {
+            yield a + b;
+            mem::swap(&mut a, &mut b);
+            b = a + b;
+        }
+
+    };
+    return create_generator(c);
+}
+
 pub fn is_prime(n: u128) -> bool {
     if n <= 1 {
         return false;
@@ -31,6 +50,29 @@ pub fn is_prime(n: u128) -> bool {
         .filter(|x| n % x == 0)
         .collect::<Vec<_>>()
         .is_empty()
+}
+
+pub fn prime_iterator() -> impl Iterator<Item=u128> {
+    let c = #[coroutine] || {
+        let mut primes: Vec<(u128, u128)> = Vec::new();
+        for num in 2_u128.. {
+            let mut is_prime = true;
+            for p in primes.iter_mut() {
+                while p.1 < num {
+                    p.1 += p.0
+                }
+                if p.1 == num {
+                    is_prime = false;
+                    break;
+                }
+            }
+            if is_prime {
+                primes.push((num, num));
+                yield num;
+            }
+        }
+    };
+    create_generator(c)
 }
 
 #[cfg(test)]
@@ -67,5 +109,31 @@ mod tests {
         assert_eq!(false, is_prime(4));
         assert_eq!(true, is_prime(5));
         assert_eq!(false, is_prime(6));
+    }
+
+    
+    #[test]
+    fn test_prime_iterator() {
+        let mut i = prime_iterator();
+        assert_eq!(Some(2), i.next());
+        assert_eq!(Some(3), i.next());
+        assert_eq!(Some(5), i.next());
+        assert_eq!(Some(7), i.next());
+        assert_eq!(Some(11), i.next());
+        assert_eq!(Some(13), i.next());
+    }
+    
+    #[test]
+    fn test_fib_iterator() {
+        let mut i = fib_iterator();
+        assert_eq!(Some(0), i.next());
+        assert_eq!(Some(1), i.next());
+        assert_eq!(Some(1), i.next());
+        assert_eq!(Some(2), i.next());
+        assert_eq!(Some(3), i.next());
+        assert_eq!(Some(5), i.next());
+        assert_eq!(Some(8), i.next());
+        assert_eq!(Some(13), i.next());
+        assert_eq!(Some(21), i.next());
     }
 }
